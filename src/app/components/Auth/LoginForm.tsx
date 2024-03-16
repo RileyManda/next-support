55f9188d3ae137b55644a44947375d37b793686b
RileyManda <rileymanda69@gmail.com>
2024-03-16 21:01:22 +0200

    Updated SignInform

--- src/app/components/Auth/LoginForm.tsx
+++ src/app/components/Auth/LoginForm.tsx
@@ -1,1 +1,35 @@
-// auth form component
+import Image from "next/image";
+
+type User =
+  | {
+      name?: string | null | undefined;
+      email?: string | null | undefined;
+      image?: string | null | undefined;
+    }
+  | undefined;
+
+type Props = {
+  user: User;
+  pagetype: string;
+};
+
+export default function Card({ user, pagetype }: Props) {
+  const welcome = user?.name ? <div>Hello {user?.name}!</div> : null;
+  const userImage = user?.image ? (
+    <Image
+      src={user?.image}
+      width={200}
+      height={200}
+      alt={user?.name || ""}
+      priority={true}
+    />
+  ) : null;
+
+  return (
+    <div>
+      {welcome}
+      {userImage}
+      <p>{pagetype} Page!</p>
+    </div>
+  );
+}
