e182e2bc293d108dd99051c4acfc0c8bd8da6daf
RileyManda <rileymanda69@gmail.com>
2024-03-16 21:02:28 +0200

    Implemented signin form to main view

---
+++ src/app/server/page.tsx
@@ -0,0 +1,18 @@
+import { options } from  "../api/auth/[...nextauth]/options";
+import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth/next";
+import SignInForm from "../components/SignInForm";
+
+export default async function ServerPage() {
+  const session = await getServerSession(options);
+
+  if (!session) {
+    redirect("/api/auth/signin?callbackUrl=/server");
+  }
+
+  return (
+    <div>
+      <SignInForm user={session?.user} pagetype={"Server"} />
+    </div>
+  );
+}
